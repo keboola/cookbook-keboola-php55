acbd553cd72b953c4e0ee39d6efd707da1b89bb6
Martin Halamíček <martin@halamicek.cz>
2014-10-31 15:17:07 +0100

    init

---
+++ attributes/default.rb
@@ -0,0 +1,5 @@
+
+default['php']['packages'] = %w{ php55 php55-devel php-pear php55-pdo php55-mysql php55-mbstring php55-pgsql php55-mcrypt php55-pecl-apcu }
+default['php']['directives'] = {
+    'date.timezone' => 'Europe/Prague'
+}
